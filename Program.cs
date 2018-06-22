024ad52442eb27a53fcc790db2486461ce88c432
Michael Linder <michael@strali.solutions>
2018-06-22 20:45:19 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -43,13 +43,22 @@ namespace IFX.ECCDHTest
 
         public static void ECDHKeyExchangeExample()
         {
+            /*NOTE: this should represent an example of ECDH. As there are no mechanisms used in order to ensure if Alice is Alice and Bob is Bob as now
+             * certificate is used in this example. */
 
-            //a public key for testing in Hex
+            /* Define used key exchange algorithm. In our case this is elliptic curve diffie hellmann. */
+            const string Algorithm = "ECDH";
+
+            /* ALICE starts the key exchange. She sends the the X and Y coordinates of her public key as well as the used curve to BOB */
+
             const string Alicebase16strpubX = "0x14CC3B7FBEF441E21DE27CA72F5E2BB60EFEA474A5973028589016D36DB11E267A1F49FD2DC1F42553E0A6BB4E66CA9E8C2667074A7EABAD1A10545626B53F4ECC9";
             const string Alicebase16strY = "0x190E33894B32DD6FDFDF8E560630B2419CC45A7FF770530CD564354A5D4D7E76DB1F4A1C0DC9E7D5720F257C5A8D2D908C342217300ACD78D258D00EEDDB2C441F5";
             const string curve = "P-521";//"SECP521R1";
-            const string Algorithm = "ECDH";
 
+            /****************************************************************************/
+            /* BOB starts the actions. Bob uses the public public key coordinates as well as the curve to generate an ephemeral key
+               pair on his side.
+            */
 
             X9ECParameters ecP = NistNamedCurves.GetByName(curve);
 
@@ -65,30 +74,29 @@ namespace IFX.ECCDHTest
             KeyGenerationParameters keygenParams = new ECKeyGenerationParameters(new ECDomainParameters(ecP.Curve, ecP.G, ecP.N), new SecureRandom());
 
             KeyGen.Init(keygenParams);
-            //DHParameters bobParameters = aliceGenerator.GenerateParameters();
 
             AsymmetricCipherKeyPair KeyPair = KeyGen.GenerateKeyPair();
             IBasicAgreement KeyAgree = AgreementUtilities.GetBasicAgreement(Algorithm);
+            /*****************************************************************************/
+
+            /* BOB calculates the SHARED SECRET */
             KeyAgree.Init(KeyPair.Private);
-            //END SETUP BOB
+            BigInteger Agree = KeyAgree.CalculateAgreement(xxpk);
+            /*****************************************************************************/
 
+            /*BOB encrypts his secret message. Using the Encryption helper method with AES 256 CBC. The helper method is using the last
+            16 bytes of the generated secret and a generated initial vector of 16 byte to encrypt*/
+            string ciphertext = Encryption.EncryptString("Hallo Alice, this is an important message to you.", Agree.ToByteArray());
             ECPublicKeyParameters params2 = (ECPublicKeyParameters)KeyPair.Public;
+            /*****************************************************************************/
 
+            /*BOB is sending the x and y coordinates of his public key and the encrypted message to Alice on a public (not secured) channel*/
             Console.WriteLine("PublicKeyX Base(16): " + params2.Q.XCoord.ToBigInteger().ToString(16).ToUpper());
             Console.WriteLine("PublicKeyY Base(16): " + params2.Q.YCoord.ToBigInteger().ToString(16).ToUpper());
-
-            //BigInteger aliceAgree = aliceKeyAgree.CalculateAgreement(bobKeyPair.Public);
-            BigInteger Agree = KeyAgree.CalculateAgreement(xxpk);
-            Console.WriteLine("Shared Secrect Base(16):" + Agree.ToString(16).ToUpper());
-
-            string ciphertext = Encryption.EncryptString("Test", Agree.ToByteArray()); //last 32 bytes key //16 bytes IV
             Console.WriteLine("Ciphertext: " + ciphertext);
-
-            string cleartext = Encryption.DecryptString(ciphertext, Agree.ToByteArray());
-            Console.WriteLine("Cleartext: " + cleartext);
+            /*****************************************************************************/
 
         }
     }
 
 }
-  
