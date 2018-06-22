e8cc01e06ea050d0b9fdd3514c97125e4ee3022c
achildrenmile <michael@strali.solutions>
2018-06-22 20:17:47 +0200

    Initial

---
+++ Encryption.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IFX.ECCDHTest
+{
+    public static class Encryption
+    {
+        public static string EncryptString(string text, byte[] key)
+        {
+            
+            using (var aesAlg = Aes.Create())
+            {
+                using (var encryptor = aesAlg.CreateEncryptor(EnsureKeySize(key), aesAlg.IV))
+                {
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+
+                        var iv = aesAlg.IV;
+
+                        var decryptedContent = msEncrypt.ToArray();
+
+                        var result = new byte[iv.Length + decryptedContent.Length];
+
+                        Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                        Buffer.BlockCopy(decryptedContent, 0, result, iv.Length, decryptedContent.Length);
+
+                        return Convert.ToBase64String(result);
+                    }
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText, byte[] key)
+        {
+            var fullCipher = Convert.FromBase64String(cipherText);
+
+            var iv = new byte[16];
+            var cipher = new byte[16];
+
+            Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+            Buffer.BlockCopy(fullCipher, iv.Length, cipher, 0, iv.Length);
+           
+            using (var aesAlg = Aes.Create())
+            {
+                using (var decryptor = aesAlg.CreateDecryptor(EnsureKeySize(key), iv))
+                {
+                    string result;
+                    using (var msDecrypt = new MemoryStream(cipher))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                result = srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+            }
+        }
+
+        private static byte[] EnsureKeySize(byte[] key)
+        {
+            byte[] ret=new byte[16];
+
+            if(key.Length>16)
+            {
+                for (int i = key.Length-1, j = 0; i >= (key.Length - 16); i--, j++)
+                {
+                    ret[j] = key[i];
+                }
+            }
+
+            return ret;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Agreement;
+using Org.BouncyCastle.Crypto.Agreement.Kdf;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Asn1.X509;
+using System.Diagnostics;
+using Org.BouncyCastle.OpenSsl;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Asn1.Pkcs;
+using System.Globalization;
+using Org.BouncyCastle.Asn1.Sec;
+using Org.BouncyCastle.Asn1.Eac;
+using Org.BouncyCastle.Asn1.BC;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Math.EC;
+
+namespace IFX.ECCDHTest
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //TestMethod();
+            ECDHKeyExchangeExample();
+            Console.Read();
+        }
+
+        public static void ECDHKeyExchangeExample()
+        {
+
+            //a public key for testing in Hex
+            const string Alicebase16strpubX = "0x14CC3B7FBEF441E21DE27CA72F5E2BB60EFEA474A5973028589016D36DB11E267A1F49FD2DC1F42553E0A6BB4E66CA9E8C2667074A7EABAD1A10545626B53F4ECC9";
+            const string Alicebase16strY = "0x190E33894B32DD6FDFDF8E560630B2419CC45A7FF770530CD564354A5D4D7E76DB1F4A1C0DC9E7D5720F257C5A8D2D908C342217300ACD78D258D00EEDDB2C441F5";
+            const string curve = "P-521";//"SECP521R1";
+            const string Algorithm = "ECDH";
+
+
+            X9ECParameters ecP = NistNamedCurves.GetByName(curve);
+
+            FpCurve c = (FpCurve)ecP.Curve;
+
+            ECFieldElement x = c.FromBigInteger(new BigInteger(System.Numerics.BigInteger.Parse(Alicebase16strpubX, NumberStyles.HexNumber).ToString()));
+            ECFieldElement y = c.FromBigInteger(new BigInteger(System.Numerics.BigInteger.Parse(Alicebase16strY, NumberStyles.HexNumber).ToString()));
+
+            Org.BouncyCastle.Math.EC.ECPoint q = new FpPoint(c, x, y);
+            ECPublicKeyParameters xxpk = new ECPublicKeyParameters("ECDH", q, SecObjectIdentifiers.SecP521r1);
+
+            IAsymmetricCipherKeyPairGenerator KeyGen = GeneratorUtilities.GetKeyPairGenerator(Algorithm);
+            KeyGenerationParameters keygenParams = new ECKeyGenerationParameters(new ECDomainParameters(ecP.Curve, ecP.G, ecP.N), new SecureRandom());
+
+            KeyGen.Init(keygenParams);
+            //DHParameters bobParameters = aliceGenerator.GenerateParameters();
+
+            AsymmetricCipherKeyPair KeyPair = KeyGen.GenerateKeyPair();
+            IBasicAgreement KeyAgree = AgreementUtilities.GetBasicAgreement(Algorithm);
+            KeyAgree.Init(KeyPair.Private);
+            //END SETUP BOB
+
+            ECPublicKeyParameters params2 = (ECPublicKeyParameters)KeyPair.Public;
+
+            Console.WriteLine("PublicKeyX Base(16): " + params2.Q.XCoord.ToBigInteger().ToString(16).ToUpper());
+            Console.WriteLine("PublicKeyY Base(16): " + params2.Q.YCoord.ToBigInteger().ToString(16).ToUpper());
+
+            //BigInteger aliceAgree = aliceKeyAgree.CalculateAgreement(bobKeyPair.Public);
+            BigInteger Agree = KeyAgree.CalculateAgreement(xxpk);
+            Console.WriteLine("Shared Secrect Base(16):" + Agree.ToString(16).ToUpper());
+
+            string ciphertext = Encryption.EncryptString("Test", Agree.ToByteArray()); //last 32 bytes key //16 bytes IV
+            Console.WriteLine("Ciphertext: " + ciphertext);
+
+            string cleartext = Encryption.DecryptString(ciphertext, Agree.ToByteArray());
+            Console.WriteLine("Cleartext: " + cleartext);
+
+        }
+    }
+
+}
+  
